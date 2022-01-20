eca994d8146f273c44f909c2178feb50443fcd99
AkshayHari007 <akshayhari321@gmail.com>
2022-01-20 23:31:29 +0530

    Sign-up Validation Completed

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -19,7 +19,7 @@ function emailValidate() {
         em.innerHTML = "Email format should be in example@domain.xxx or example@domain.xxx.xxx";
         em.style.fontSize = "14px";
         em.style.color = "red";
-        e.style.border = "2px solid red";
+        e.style.border = "3px solid red";
         ev = false;
     }
 }
@@ -35,7 +35,57 @@ function phValidate() {
         phm.innerHTML = "Phone number format should be in XXX-XXX-XXXX, XXX.XXX.XXXX, XXX XXX XXXX or XXXXXXXXXX";
         phm.style.fontSize = "14px";
         phm.style.color = "red";
-        ph.style.border = "2px solid red";
+        ph.style.border = "3px solid red";
         phv = false;
+    }
+}
+
+function pwdValidate() {
+    var leg = pwd.value.length;
+    var L = /[a-z]/;
+    var U = /[A-Z]/;
+    var N = /[0-9]/;
+
+    if ((leg >= 8) && (L.test(pwd.value)) && (U.test(pwd.value)) && (N.test(pwd.value))) {
+        pwdm.innerHTML = "Valid";
+        pwdm.style.color = "green";
+        pwdv = true;
+        if ((leg >= 8) && (leg <= 12)) {
+            pwd.style.border = "3px solid orange";
+        } else {
+            pwd.style.border = "3px solid green";
+        }
+    } else {
+        pwdm.innerHTML = "Password should contain minimum 8 characters and at least one uppercase, one lowercase and a number";
+        pwdm.style.fontSize = "14px";
+        pwdm.style.color = "red";
+        pwd.style.border = "3px solid red";
+        pwdv = false;
+
+    }
+}
+
+function pwdcValidate() {
+    if ((pwd.value) == (pwdc.value)) {
+        pwdcm.innerHTML = "Both the passwords are matching";
+        pwdcm.style.color = "green";
+        pwdc.style.border = "";
+        pwdcv = true;
+    } else {
+        pwdcm.innerHTML = "Entered passwords is not matching the above one!";
+        pwdcm.style.fontSize = "14px";
+        pwdcm.style.color = "red";
+        pwdc.style.border = "3px solid red";
+        pwdcv = false;
+    }
+}
+
+function validat() {
+    if (ev && phv && pwdv && pwdcv) {
+        alert("Your account has been successfully created. Press OK to enter the LogIn page.");
+        return true;
+    } else {
+        alert("Error!");
+        return false;
     }
 }
